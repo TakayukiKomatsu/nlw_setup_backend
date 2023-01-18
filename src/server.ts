1a3d2c15fd0fd7079dd166b6776f215e2fb8b955
Takayuki Komatsu <komatsutakayuki235@live.com>
2023-01-18 20:28:13 -0300

    chore: configure cors

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import {PrismaClient} from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+  return await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "Beber"
+      }
+    }
+  })
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log("The server started on port 3333 successfully.")
+})
